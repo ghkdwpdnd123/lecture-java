05e185fb480c8f0c49ecfb8ff2a9f0e68d2599c5
Ohhanseul <143672993+Ohhanseul@users.noreply.github.com>
2024-10-11 14:19:16 +0900

    commit

--- download/java-spring-boot/agron_test/thymeleaf_test/src/main/java/com/haji/demo/thymeleaf_test/controller/HtmlController.java
+++ download/java-spring-boot/agron_test/thymeleaf_test/src/main/java/com/haji/demo/thymeleaf_test/controller/HtmlController.java
@@ -2,10 +2,6 @@ package com.haji.demo.thymeleaf_test.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @Controller
 public class HtmlController {
@@ -22,5 +18,10 @@ public class HtmlController {
     @GetMapping("/ecommerce")
     public String ecommerce() {
         return "pages/ecommerce/overview.html";
+    }
+
+    @GetMapping("/addLemps")
+    public String addViewLemp() {
+        return "addViewLemp"; 
     }
 }
